584a532d89db971918723f622ebf87f82be2eea4
starx04 <antonifernandez207@gmail.com>
2026-06-10 10:30:00 -0500

    Creacion de los modulos de usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { PerfilModule } from './perfil/perfil.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsuariosModule, PerfilModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
