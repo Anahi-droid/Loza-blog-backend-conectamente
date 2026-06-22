5701b54acfc27f5a050391ac468de82e16504907
Anahi-droid <lozaanahi02@gmail.com>
2026-06-22 17:25:54 -0500

    Módulos de citas, usuarios, psicólogos y agenda con pruebas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CitasModule } from './citas/citas.module';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { PsicologosModule } from './psicologos/psicologos.module';
+import { AgendaModule } from './agenda/agenda.module';
 
 @Module({
-  imports: [],
+  imports: [CitasModule, UsuariosModule, PsicologosModule, AgendaModule],
   controllers: [AppController],
   providers: [AppService],
 })
