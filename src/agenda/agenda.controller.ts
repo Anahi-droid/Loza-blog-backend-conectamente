0020653169b2a4a09e7d7df0cca13874a02fb0fe
Anahi-droid <lozaanahi02@gmail.com>
2026-06-24 22:13:44 -0500

    Creación controller citas/agenda

--- src/agenda/agenda.controller.ts
+++ src/agenda/agenda.controller.ts
@@ -1,25 +1,20 @@
-import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
-import { PsicologosService } from '../psicologos/psicologos.service';
-import { Psicologo } from '../psicologos/psicologo.entity';
+import { Controller, Get, Post, Body, Param, Req, UseGuards } from '@nestjs/common';
+import { AgendasService } from './agenda.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
-@Controller('psicologos')
-export class PsicologosController {
-  constructor(private readonly psicologosService: PsicologosService) {}
-
-  
-  @Get()
-  listarTodos() {
-    return this.psicologosService.listarTodos();
-  }
-
+@Controller('agendas')
+export class AgendasController {
+  constructor(private readonly agendasService: AgendasService) {}
   
   @UseGuards(JwtAuthGuard)
-  @Post('perfil/:usuarioId')
-  crearPerfil(
-    @Param('usuarioId') usuarioId: string,
-    @Body() datosProf: Partial<Psicologo>
-  ) {
-    return this.psicologosService.crearPerfil(usuarioId, datosProf);
+  @Post()
+  async crearHorario(@Req() req, @Body('fechaHoraInicio') fechaHoraInicio: string) {
+    const psicologoId = req.user.psicologoId; 
+    return this.agendasService.crearDisponibilidad(psicologoId, new Date(fechaHoraInicio));
+  }
+
+  @Get('psicologo/:id')
+  obtenerDisponibles(@Param('id') psicologoId: string) {
+    return this.agendasService.obtenerDisponiblesPorPsicologo(psicologoId);
   }
 }
--- src/citas/citas.controller.ts
+++ src/citas/citas.controller.ts
@@ -7,18 +7,16 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 export class CitasController {
   constructor(private readonly citasService: CitasService) {}
 
-  
   @Post()
   async reservarCita(
     @Req() req,
     @Body('agendaId') agendaId: string,
     @Body('motivoConsulta') motivoConsulta: string,
   ) {
-    const pacienteId = req.user.id; // ID extraído del token JWT
+    const pacienteId = req.user.id; 
     return this.citasService.agendarCita(pacienteId, agendaId, motivoConsulta);
   }
 
-  
   @Get('mis-citas')
   async listarMisCitas(@Req() req) {
     const usuarioId = req.user.id;
