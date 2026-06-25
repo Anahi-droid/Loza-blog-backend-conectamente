d4e122f722f9ec5135a2de1cb67ccaaedfc59e80
Anahi-droid <lozaanahi02@gmail.com>
2026-06-24 21:59:06 -0500

    Implementación psicologos.controller

--- src/psicologos/psicologos.controller.ts
+++ src/psicologos/psicologos.controller.ts
@@ -1,34 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { PsicologosService } from './psicologos.service';
-import { CreatePsicologoDto } from './dto/create-psicologo.dto';
-import { UpdatePsicologoDto } from './dto/update-psicologo.dto';
+import { Psicologo } from '../psicologos/psicologo.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('psicologos')
 export class PsicologosController {
   constructor(private readonly psicologosService: PsicologosService) {}
 
-  @Post()
-  create(@Body() createPsicologoDto: CreatePsicologoDto) {
-    return this.psicologosService.create(createPsicologoDto);
-  }
 
   @Get()
-  findAll() {
-    return this.psicologosService.findAll();
+  listarTodos() {
+    return this.psicologosService.listarTodos();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.psicologosService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePsicologoDto: UpdatePsicologoDto) {
-    return this.psicologosService.update(+id, updatePsicologoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.psicologosService.remove(+id);
+  @UseGuards(JwtAuthGuard)
+  @Post('perfil/:usuarioId')
+  crearPerfil(
+    @Param('usuarioId') usuarioId: string,
+    @Body() datosProf: Partial<Psicologo>
+  ) {
+    return this.psicologosService.crearPerfil(usuarioId, datosProf);
   }
 }
