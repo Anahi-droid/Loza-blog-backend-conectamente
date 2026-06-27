cf644140839550dd7730bf83053ae14b4a51c04e
Anahi-droid <lozaanahi02@gmail.com>
2026-06-26 22:42:07 -0500

    Corecciónes módulo agenda

--- src/agenda/agenda.module.ts
+++ src/agenda/agenda.module.ts
@@ -1,15 +1,17 @@
+// src/agenda/agenda.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgendasService } from './agenda.service';
 import { AgendasController } from './agenda.controller';
 import { Agenda } from './agenda.entity';
+import { DisponibilidadExcepcion } from '../psicologos/disponibilidad-excepcion.entity'; 
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Agenda]), 
+    TypeOrmModule.forFeature([Agenda, DisponibilidadExcepcion]), 
   ],
   controllers: [AgendasController],
   providers: [AgendasService],
-  exports: [AgendasService], 
+  exports: [AgendasService],
 })
-export class AgendasModule {}
+export class AgendaModule {}
--- src/agenda/agenda.service.ts
+++ src/agenda/agenda.service.ts
@@ -1,17 +1,36 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+// src/agenda/agenda.service.ts
+import { Injectable, ConflictException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Agenda } from './agenda.entity';
+import { DisponibilidadExcepcion } from '../psicologos/disponibilidad-excepcion.entity'; 
 
 @Injectable()
 export class AgendasService {
   constructor(
     @InjectRepository(Agenda)
-    private agendaRepository: Repository<Agenda>,
+    private readonly agendaRepository: Repository<Agenda>,
+    @InjectRepository(DisponibilidadExcepcion)
+    private readonly excepcionRepository: Repository<DisponibilidadExcepcion>,
   ) {}
 
-  
+  async verificarExcepcion(psicologoId: string, fechaDeseada: Date): Promise<boolean> {
+    const excepcionBloqueante = await this.excepcionRepository.createQueryBuilder('excepcion')
+      .where('excepcion.psicologo_id = :psicologoId', { psicologoId })
+      .andWhere(':fecha >= excepcion.fechaInicio AND :fecha <= excepcion.fechaFin', { fecha: fechaDeseada })
+      .getOne();
+
+    if (excepcionBloqueante) {
+      throw new BadRequestException(
+        `El psicólogo no está disponible en este horario. Motivo: ${excepcionBloqueante.motivo}`
+      );
+    }
+    return true;
+  }
+
   async crearDisponibilidad(psicologoId: string, fechaHora: Date): Promise<Agenda> {
+    await this.verificarExcepcion(psicologoId, fechaHora);
+
     const existe = await this.agendaRepository.findOne({
       where: {
         psicologo: { id: psicologoId },
@@ -31,7 +50,6 @@ export class AgendasService {
     return await this.agendaRepository.save(bloque);
   }
 
-  
   async obtenerDisponiblesPorPsicologo(psicologoId: string): Promise<Agenda[]> {
     return await this.agendaRepository.find({
       where: {
