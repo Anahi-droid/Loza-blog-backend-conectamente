6b70b1aef9d817f24c7122387bb8e64e60109e6b
Anahi-droid <lozaanahi02@gmail.com>
2026-06-27 20:59:13 -0500

    Actualización de recomendaciones post

--- src/recomendaciones/dto/create-recomendacione.dto.ts
+++ src/recomendaciones/dto/create-recomendacione.dto.ts
@@ -3,21 +3,21 @@ import { IsUUID, IsNotEmpty, IsDateString, IsString } from 'class-validator';
 export class CreateRecomendacionDto {
   @IsUUID('4', { message: 'El pacienteId debe ser un UUID válido' })
   @IsNotEmpty({ message: 'El id del paciente es requerido' })
-  pacienteId: string;
+  pacienteId?: string;
 
   @IsUUID('4', { message: 'El psicologoId debe ser un UUID válido' })
   @IsNotEmpty({ message: 'El id del psicólogo es requerido' })
-  psicologoId: string;
+  psicologoId?: string;
 
   @IsDateString({}, { message: 'La fecha debe ser una cadena ISO válida' })
   @IsNotEmpty({ message: 'La fecha de asignación es requerida' })
-  fecha: Date;
+  fecha?: Date;
 
   @IsString()
   @IsNotEmpty({ message: 'El título de la recomendación es requerido' })
-  titulo: string;
+  titulo?: string;
 
   @IsString()
   @IsNotEmpty({ message: 'La descripción no puede estar vacía' })
-  descripcion: string;
+  descripcion?: string;
 }
